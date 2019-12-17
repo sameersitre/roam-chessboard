b7ad066b57bbc1e932872fa4bed0987d0957822f
sameer sitre <sameer.sitre@schrocken.com>
2019-12-17 12:27:25 +0530

    Updates

---
+++ precache-manifest.f05a58c71e2b71efe171e1027d408f61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c959f6f7f8fd60a2f5a0609e86a579",
+    "url": "/roam-chessboard/index.html"
+  },
+  {
+    "revision": "4cc60768a53a5fe73ac8",
+    "url": "/roam-chessboard/static/css/main.7a427453.chunk.css"
+  },
+  {
+    "revision": "1ed065644bdef3d84099",
+    "url": "/roam-chessboard/static/js/2.029493da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/roam-chessboard/static/js/2.029493da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cc60768a53a5fe73ac8",
+    "url": "/roam-chessboard/static/js/main.f626bbf5.chunk.js"
+  },
+  {
+    "revision": "4cbd233cacdeb8e41439",
+    "url": "/roam-chessboard/static/js/runtime-main.dac8257a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roam-chessboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
